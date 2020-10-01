945561a64956fe8155f55ac1400a35a852e5dba9
Jim Lusk <jblusk@gmail.com>
2020-09-30 20:17:25 -0700

    Added a one second interval console log print to the script.

--- timers.js
+++ timers.js
@@ -1,8 +1,21 @@
 //Exercise to demonstrate use of thread control/timeout.
 
-const waitTime=3000;
+const waitTime=5000;
+const waitInterval=1000;
+
+
+let currentTime = 0;
 console.log(`Setting a ${waitTime / 1000} second delay`);
 
-const timerFinished = () => console.log("done");
+const timerFinished = () => {
+    console.log("done");
+    clearInterval(interval);
+}
+
+const incrementTime = () => {
+    currentTime += waitInterval;
+    console.log(`Waiting ${currentTime/1000} seconds`);
+}
 
 setTimeout(timerFinished, waitTime);
+const interval = setInterval(incrementTime, waitInterval);
