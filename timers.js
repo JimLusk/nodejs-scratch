914879fef29e675f248da7435db42a6dac116497
Jim Lusk <jblusk@gmail.com>
2020-09-30 20:28:25 -0700

    Added a dynamic command line progress indicator using JavaScript interval.

--- timers.js
+++ timers.js
@@ -8,13 +8,18 @@ let currentTime = 0;
 console.log(`Setting a ${waitTime / 1000} second delay`);
 
 const timerFinished = () => {
-    console.log("done");
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    console.log(`Done`);
     clearInterval(interval);
 }
 
 const incrementTime = () => {
     currentTime += waitInterval;
-    console.log(`Waiting ${currentTime/1000} seconds`);
+    const p = Math.floor ((currentTime / waitTime) * 100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Waiting...${p}%`);
 }
 
 setTimeout(timerFinished, waitTime);
